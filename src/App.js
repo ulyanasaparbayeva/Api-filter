19ba0c92e8e5d382e656180aa0224f9dcc8c5b78
Ulyana Saparbayeva <ulyanasaparbayeva@gmail.com>
2023-08-24 22:23:26 +0500

    updated first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Api from './component/Api'
+
+
+
+function App() {
+  return (
+      <div className="App">
+      <Api/>
+      </div>
+  );
+}
+
+export default App;
